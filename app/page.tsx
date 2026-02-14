0b6a13e772218cf95ee4b2fdc431e0b76c9df4b0
lwoitr <zaoxi3056@gmail.com>
2026-02-14 21:07:46 +0300

    deployfix1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { MinerProvider } from "./context/MinerContext/MinerContext";
 import Dashboard from "./dashboard/page";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <MinerProvider>
